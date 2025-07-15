2238e9a28a0159d464f827dcfe7f483d91813abf
Akhil2195 <42337580+Akhil2195@users.noreply.github.com>
2025-07-15 20:15:59 +0530

    Update test-setup.js

--- src/test/test-setup.js
+++ src/test/test-setup.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 beforeAll(async () => {
   const dbUri = "mongodb+srv://akhilraishetti21:ZIskZ9vYzFDvmhCx@cluster0.k8hw2z8.mongodb.net/ecommerce"; // or use your actual URI
   const db = await mongoose.connect(dbUri);
-  console.log(`db`, db);
+  console.log(`db`, db.connection);
 }, 15000); // Give 15s for slow startups
 
 afterAll(async () => {
